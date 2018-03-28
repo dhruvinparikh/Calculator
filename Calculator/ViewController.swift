f451267d9e43a02e687cf2f239eda180e153ad63
Dhruvin Parikh <n01150847@humbermail.ca>
2018-03-28 09:38:33 -0400

    Initial Commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var num1TF: UITextField!
+    @IBOutlet weak var num2TF: UITextField!
+    @IBAction func btnDIV(_ sender: Any) {
+        if(num1TF.text != "" && num2TF.text != ""){
+            let num1 = Double(num1TF.text!)
+            let num2 = Double(num2TF.text!)
+            if(num2 != 0){
+            resultlbl.text = "Result : "+String(num1! / num2!)
+            }
+            else{
+                resultlbl.text = "Cannot divide by 0"
+            }
+        }
+        else{
+            resultlbl.text = "Please enter digit"
+        }
+    }
+    @IBAction func btnMUL(_ sender: Any) {
+        if(num1TF.text != "" && num2TF.text != ""){
+            let num1 = Double(num1TF.text!)
+            let num2 = Double(num2TF.text!)
+            resultlbl.text = "Result : "+String(num1! * num2!)
+        }
+        else{
+            resultlbl.text = "Please enter digit"
+        }
+    }
+    @IBAction func btnSUB(_ sender: Any) {
+        if(num1TF.text != "" && num2TF.text != ""){
+            let num1 = Double(num1TF.text!)
+            let num2 = Double(num2TF.text!)
+            resultlbl.text = "Result : "+String(num1! - num2!)
+        }
+        else{
+            resultlbl.text = "Please enter digit"
+        }
+    }
+    
+    @IBAction func btnADD(_ sender: Any) {
+        if(num1TF.text != "" && num2TF.text != ""){
+        let num1 = Double(num1TF.text!)
+        let num2 = Double(num2TF.text!)
+            resultlbl.text = "Result : "+String(num1! + num2!)
+        }
+        else{
+            resultlbl.text = "Please enter digit"
+        }
+    }
+    @IBOutlet weak var resultlbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
